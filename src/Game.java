33e8242bbcb2ed3b6abda0fc007b0f99e4474ce0
MChandraKishore <chandrakishore518@gmail.com>
2018-10-08 22:36:43 +0530

    Added motion

--- src/Game.java
+++ src/Game.java
@@ -17,7 +17,8 @@ import java.util.Random;
 public class Game extends JFrame {
 
 	private JPanel contentPane;
-
+	int Player1x=770;
+	int Player1y=450;
 	/**
 	 * Launch the application.
 	 */
@@ -44,6 +45,12 @@ public class Game extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+
+		JLabel lblPlayer1 = new JLabel();
+		ImageIcon player1 = new ImageIcon(new ImageIcon(Game.class.getResource("/img/player-1.png")).getImage().getScaledInstance(40, 80, Image.SCALE_DEFAULT));
+		lblPlayer1.setIcon(player1);
+		lblPlayer1.setBounds(Player1x, Player1y, 40, 80);
+		contentPane.add(lblPlayer1);
 		
 		JLabel lblCk = new JLabel();
 		ImageIcon board = new ImageIcon(new ImageIcon(Game.class.getResource("/img/board.jpg")).getImage().getScaledInstance(700, 600, Image.SCALE_DEFAULT));
@@ -63,32 +70,50 @@ public class Game extends JFrame {
 		lblPlayerTurn.setBounds(755, 28, 204, 34);
 		contentPane.add(lblPlayerTurn);
 		
-		JButton btnRollDice = new JButton("Roll Dice");
-		btnRollDice.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				ImageIcon dice = new ImageIcon(new ImageIcon(Game.class.getResource("/img/dice-"+RollDice()+".png")).getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT));
-				lblDice.setIcon(dice);
-			}
-		});
-		btnRollDice.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		btnRollDice.setBounds(780, 296, 142, 50);
-		contentPane.add(btnRollDice);
-		
-		JLabel lblPlayer1 = new JLabel();
-		ImageIcon player1 = new ImageIcon(new ImageIcon(Game.class.getResource("/img/player-1.png")).getImage().getScaledInstance(40, 80, Image.SCALE_DEFAULT));
-		lblPlayer1.setIcon(player1);
-		lblPlayer1.setBounds(770, 450, 40, 80);
-		contentPane.add(lblPlayer1);
 		
 		JLabel lblPlayer2 = new JLabel();
 		ImageIcon player2 = new ImageIcon(new ImageIcon(Game.class.getResource("/img/player-2.png")).getImage().getScaledInstance(40, 80, Image.SCALE_DEFAULT));
 		lblPlayer2.setIcon(player2);
 		lblPlayer2.setBounds(830, 450, 40, 80);
 		contentPane.add(lblPlayer2);
+		
+		JButton btnRollDice = new JButton("Roll Dice");
+		btnRollDice.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				int diceroll=RollDice();
+				ImageIcon dice = new ImageIcon(new ImageIcon(Game.class.getResource("/img/dice-"+diceroll+".png")).getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT));
+				lblDice.setIcon(dice);
+				MovePlayer1(lblPlayer1,diceroll);
+				
+			}
+		});
+		btnRollDice.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		btnRollDice.setBounds(780, 296, 142, 50);
+		contentPane.add(btnRollDice);
+		
+		
 	}
 	public int RollDice() {
 		Random num=new Random();
-		return num.nextInt(6);
+		int rnum=num.nextInt(6);
+		while(rnum==0) {
+			rnum=num.nextInt(6);
+		}
+		return rnum;
+	}
+	public void MovePlayer1(JLabel obj,int position) {
+		if(Player1x==770&&Player1y==450) {
+			Player1x=20+70*(position-1);
+			Player1y=480;
+		}
+		else{
+			Player1x+=70*position;
+			if(Player1x>700) {
+				Player1x=Player1x%700;
+				Player1y-=60;
+			}
+		}
+		obj.setBounds(Player1x, Player1y, 40, 80);
 	}
 }
